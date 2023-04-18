ae5ba25f8ed54ce18f83176dae5c7d0ef488afec
Sergey Akimov <qqun.y@yandex.ru>
2023-04-19 02:11:07 +0300

    Make basic html page

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,10 +1,7 @@
 #include <drogon/drogon.h>
 
 int main(int argc, char* argv[]) {
-    drogon::app().setLogPath("./logs/")
-                 .setLogLevel(trantor::Logger::kWarn)
-                 .addListener("0.0.0.0", 7777)
-                 .setThreadNum(16)
-                 .enableRunAsDaemon()
-                 .run();
+    drogon::app().loadConfigFile("configs/drogon.json");
+    drogon::app().run();
+    return 0;
 }
